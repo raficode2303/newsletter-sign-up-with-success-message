5421053ee5f50159ec8b6b992c06316c811a010e
rafi&pw-mosh <rafi-mosh@email.com>
2024-09-03 19:15:15 +0300

    show 'valid email required' ONLY if email input is NOT-FOCUSED(:not(:focus, :placeholder-shown)) + make bckgc to stay white when field is empty

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 const newsletterFormEl = document.querySelector('.newsletter-form')
 const emailInputEl = document.querySelector('.newsletter-form-email')
 const newsletterFormSubmitEl = document.querySelector('.newsletter-form-submit')
-const invalidEmailMassageEl = document.querySelector('.invalid-email')
+const invalidEmailMassageEl = document.querySelector(
+  '.invalid-email-required-text'
+)
 const newsletterImageEl = document.querySelector('.newsletter-img')
 
 // Vars
@@ -20,6 +22,8 @@ emailInputEl.addEventListener('input', () => {
     !IsMassageHidden && invalidEmailMassageEl.setAttribute('hidden', '')
   } else {
     // Email NOT valid
+    // TODO!: SHOW 'valid email required' ONLY if email input is NOT-FOCUSED(:not(:focus)) + make bckgc to stay white when field is empty.
+    // TODO!: TRY ACHIEVE IT USING CSS ONLY(with changing the layout and :not(:focus):invalid)
     console.log('email NOT valid')
     let IsMassageHidden = invalidEmailMassageEl.hasAttribute('hidden')
     IsMassageHidden && invalidEmailMassageEl.removeAttribute('hidden')
@@ -44,7 +48,13 @@ newsletterFormEl.addEventListener('submit', (e) => {
 newsletterFormSubmitEl.addEventListener('mouseover', () => {
   newsletterImageEl.classList.add('sharp-corners')
 })
+// TODO!: mouse out remove '.sharp-corners' class - DONE!
 newsletterFormSubmitEl.addEventListener('mouseout', () => {
   newsletterImageEl.classList.remove('sharp-corners')
 })
-// TODO!: mouse out remove '.sharp-corners' class
+newsletterFormSubmitEl.addEventListener('click', () => {
+  emailInputEl.classList.add('subscribed-failed')
+  setTimeout(() => {
+    emailInputEl.classList.remove('subscribed-failed')
+  }, 300)
+})
